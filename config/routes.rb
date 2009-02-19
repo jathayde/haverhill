3a11b918d15f477aa07f669c26809e962a1a084a
John Athayde <john@hyphenateme.com>
2009-02-19 16:42:13 -0500

    nav and logo layout

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ ActionController::Routing::Routes.draw do |map|
   map.root :controller => 'pages', :action => 'index'
   
   # Mapping pages for static delivery
+  map.news       'news',       :controller => 'pages', :action => 'news'
+  map.events     'events',     :controller => 'pages', :action => 'event_index'
+  map.volunteer  'volunteer',  :controller => 'pages', :action => 'volunteer'
+  map.programs   'programs',   :controller => 'pages', :action => 'programs'
+  map.services   'services',   :controller => 'pages', :action => 'services'
+  map.about      'about',      :controller => 'pages', :action => 'about'
+  map.contact    'contact',    :controller => 'pages', :action => 'contac'
 
+  # map.    :controller => 'pages', :action => ''
   
   # Install the default routes as the lowest priority.
   # map.connect ':controller/:action/:id'
