a732c6da05cfc3c1729a3313a28849e6e6cc7e7e
John Athayde <john@hyphenateme.com>
2009-02-24 12:16:16 -0500

    adding files for pages and header image group 1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,39 @@ class PagesController < ApplicationController
     @body_id = "home"
   end
   
+  def news
+    @body_id = "news"
+  end
+  
+  def event_index
+    @body_id = "events"
+  end
+  
+  def volunteer
+    @body_id = "volunteer"
+  end
+  
+  def programs
+    @body_id = "programs"
+  end
+  
+  def services
+    @body_id = "services"
+  end
+  
+  def about
+    @body_id = "about"
+  end
+  
+  def contact
+    @body_id = "contact"
+  end
+  
+  def camp
+    @body_id = "programs"
+  end
+  
+  def campreg
+    @body_id = "programs"
+  end
 end
