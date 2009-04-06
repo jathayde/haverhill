6be63ec438a2420d11f6cac66675d39638b0b1fa
Tom Copeland <tom@tom-mac.local>
2009-04-06 12:06:11 -0400

    More capification

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,30 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "haverhill"
+set :repository,  "git://github.com/boboroshi/haverhill.git"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :scm, :git
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+role :app, "haverhill.infoether.com"
+role :web, "haverhill.infoether.com"
+role :db,  "haverhill.infoether.com", :primary => true
+
+task :move_in_database_yml, :roles => :app do
+  run "cp #{deploy_to}/shared/system/database.yml #{current_path}/config/"
+end
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+after "deploy:symlink", "move_in_database_yml"
+after "deploy", "deploy:migrate"
+after "deploy", "deploy:cleanup"
