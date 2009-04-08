f1b57ed2ec92784ca74cfcc2a43db0f57d8b0335
Tom Copeland <tom@tom-mac.local>
2009-04-08 14:51:41 -0400

    Adding a staging environment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,16 @@
+set :stages, %w(staging production)
+set :default_stage, "staging"
+
+require 'capistrano/ext/multistage'
+
 set :application, "haverhill"
 set :repository,  "git://github.com/boboroshi/haverhill.git"
 
-set :deploy_to, "/var/www/#{application}"
-
 set :scm, :git
 
-role :app, "haverhill.infoether.com"
-role :web, "haverhill.infoether.com"
-role :db,  "haverhill.infoether.com", :primary => true
+role :app, "www.haverhillbgc.com"
+role :web, "www.haverhillbgc.com"
+role :db,  "www.haverhillbgc.com", :primary => true
 
 set :use_sudo, false
 set :group, "infoether"
